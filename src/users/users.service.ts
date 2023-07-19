37f68f61ecf8edfc62c7dc8d1a70a0df056c2c7e
Ashhad Khan <ashhadkhan999@gmail.com>
2023-07-19 18:40:39 +0500

    feat: all user crud operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
 
-    create(email:string, password:string){
-        const user = this.repo.create({email,password})
+    return this.repo.save(user);
+  }
 
-        return this.repo.save(user)
-    }
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.findBy({ email });
+  }
+
+  async update(id: number, attr: Partial<User>) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) throw new Error("User not found");
+
+    Object.assign(user, attr);
+    return this.repo.save(user);
+  }
+
+  async delete(id: number) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) throw new Error("User not found");
+
+    return this.repo.remove(user)
+  }
 }
