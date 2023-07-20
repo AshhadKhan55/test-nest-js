eecdcc1493d9e599e9747b01396c10dd5c901b49
Ashhad Khan <ashhadkhan999@gmail.com>
2023-07-20 18:55:20 +0500

    feat: implementation of serialize and interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
@@ -13,7 +16,6 @@ export class UsersController {
         this.usersService.create(body.email,body.password)
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
     @Get("/:id")
     findUser(@Param("id") id:string){
         return this.usersService.findOne(parseInt(id));
